0698cd49cf5e91ff61431469c76d0497bc68e9c9
rafael <rafael.nascimento@mutantbr.com>
2019-10-19 22:02:33 -0300

    Remove balance exemple

--- src/services/balanceService.ts
+++
@@ -1,7 +0,0 @@
-import { balanceRepository } from '../repositories/index';
-
-const find = () => balanceRepository.find();
-
-const create = () => balanceRepository.create();
-
-export { find, create };
--- src/types/IBalance.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-
-export default interface Ibalance extends Document {
-    phoneNumber: string;
-    data: string;
-    type: string;
-};
